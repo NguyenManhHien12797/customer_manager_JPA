71a4a47f68ae80f7c4ecb62915f8c472980050cb
NguyenManhHien12797 <nguyenhien81f@gmail.com>
2022-09-15 08:27:22 +0700

    first commit

---
+++ src/main/java/com/example/customer_management/repository/customer/ICustomerRepository.java
@@ -0,0 +1,7 @@
+package com.example.customer_management.repository.customer;
+
+import com.example.customer_management.model.Customer;
+import com.example.customer_management.repository.IGeneralRepository;
+
+public interface ICustomerRepository extends IGeneralRepository<Customer> {
+}
---
+++ src/main/java/com/example/customer_management/service/ICustomerService.java
@@ -0,0 +1,6 @@
+package com.example.customer_management.service;
+
+import com.example.customer_management.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer>{
+}
